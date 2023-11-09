bf89e4256af5ebe784fd8d6fb6d42c5ddd6340c1
Maxim Khrypunov <khrypunovmax@gmail>
2023-11-09 18:15:18 +0200

    HW--43

--- src/telran/game/ResultsTable.java
+++ src/telran/game/ResultsTable.java
@@ -1,4 +1,13 @@
 package telran.game;
 
 class ResultsTable {
-  }
+    private static final Object mutex = new Object();
+    private static int place = 1;
+
+    static void recordResult(int racerNumber, long time) {
+        synchronized (mutex) {
+            System.out.println(place + "\t\t" + racerNumber + "\t\t\t" + time);
+            place++;
+        }
+    }
+}
