4f84eb2b9973a0ad7cefaeb0e7501a7fb6b17cf4
Maxim Khrypunov <khrypunovmax@gmail>
2023-11-12 13:19:09 +0200

    HW-44-1

--- src/telran/game/Race.java
+++ src/telran/game/Race.java
@@ -1,27 +1,32 @@
 package telran.game;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Race {
-    private int distance;
-    private int winner = -1;
-
-    public Race(int distance) {
-        this.distance = distance;
-    }
-
-    public int getWinner() {
-        return winner;
-    }
-
-    public void setWinner(int winner) {
-        if (this.winner == -1) {
-            this.winner = winner;
-        }
-    }
-
-    public int getDistance() {
-        return distance;
-    }
+	private int distance;
+	private int minSleep;
+	private int maxSleep;
+	AtomicInteger winner = new AtomicInteger(-1);
+	public Race(int distance, int minSleep, int maxSleep) {
+		this.distance = distance;
+		this.minSleep = minSleep;
+		this.maxSleep = maxSleep;
+	}
+	public int getWinner() {
+		return winner.get();
+	}
+	
+	public int getDistance() {
+		return distance;
+	}
+	public int getMinSleep() {
+		return minSleep;
+	}
+	public int getMaxSleep() {
+		return maxSleep;
+	}
+	
 }
-
--- src/telran/game/RaceAppl.java
+++ src/telran/game/RaceAppl.java
@@ -1,5 +1,8 @@
 package telran.game;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -9,67 +12,61 @@ import telran.view.Item;
 import telran.view.Menu;
 
 public class RaceAppl {
-    private static final int MAX_THREADS = 10;
-    private static final int MIN_THREADS = 3;
-    private static final int MIN_DISTANCE = 100;
-    private static final int MAX_DISTANCE = 2000;
 
+	private static final int MAX_THREADS = 10;
+	private static final int MIN_THREADS = 3;
+	private static final int MIN_DISTANCE = 100;
+	private static final int MAX_DISTANCE = 3500;
+	private static final int MIN_SLEEP = 2;
+	private static final int MAX_SLEEP = 5;
+	public static void main(String[] args) {
+		InputOutput io = new ConsoleInputOutput();
+		Item[] items = getItems();
+		Menu menu = new Menu("Race Game", items);
+		menu.perform(io);
 
-    public static void main(String[] args) {
-        InputOutput io = new ConsoleInputOutput();
-        Item[] items = getItems();
-        Menu menu = new Menu("Threads Race Game", items);
-        menu.perform(io);
-    }
+	}
 
-    private static Item[] getItems() {
-        Item[] res = {
-                Item.of("Start new game", RaceAppl::startGame),
-                Item.exit()
-        };
-        return res;
-    }
+	private static Item[] getItems() {
+		Item[] res = {
+				Item.of("Start new game", RaceAppl::startGame),
+				Item.exit()
+		};
+		return res;
+	}
+	static void startGame(InputOutput io) {
+		int nThreads = io.readInt("Enter number of the runners","Wrong number of the runners",
+				MIN_THREADS, MAX_THREADS);
+		int distance = io.readInt("Enter distance", "Wrong Distance",MIN_DISTANCE, MAX_DISTANCE);
+		Race race = new Race(distance, MIN_SLEEP, MAX_SLEEP);
+		Runner[] runners = new Runner[nThreads];
+		startRunners(runners, race);
+		joinRunners(runners);
+		displayWinner(race);
+	}
 
-    static void startGame(InputOutput io) {
-        int numberOfThreads = io.readInt("Enter number of the runners", "Wrong number of runners",
-                MIN_THREADS, MAX_THREADS);
-        int distance = io.readInt("Enter distance", "Wrong Distance", MIN_DISTANCE, MAX_DISTANCE);
-        Race race = new Race(distance);
-        Racer[] runners = new Racer[numberOfThreads];
-        displayResultsHeader(io);
-        startRunners(runners, race);
-        joinRunners(runners);
-        displayResults(runners);
-    }
+	private static void displayWinner(Race race) {
+		System.out.println("Congratulations to runner " + race.getWinner());
+		
+	}
 
-    private static void displayResults(Racer[] runners) {
-        for (Racer racer : runners) {
-            try {
-                racer.join(); 
-            } catch (InterruptedException e) {
-                throw new IllegalStateException();
-            }
-        }
-    }
+	private static void joinRunners(Runner[] runners) {
+		IntStream.range(0, runners.length).forEach(i -> {
+			try {
+				runners[i].join();
+			} catch (InterruptedException e) {
+				throw new IllegalStateException();
+			}
+		});
+		
+	}
 
-    private static void displayResultsHeader(InputOutput io) {
-       io.writeLine("Place\t\tThread Number\t\tTime (ms)");
-    }
+	private static void startRunners(Runner[] runners, Race race) {
+		IntStream.range(0, runners.length).forEach(i -> {
+			runners[i] = new Runner(race, i + 1);
+			runners[i].start();
+		});
+		
+	}
 
-    private static void joinRunners(Racer[] racers) {
-        for (Racer racer : racers) {
-            try {
-                racer.join();
-            } catch (InterruptedException e) {
-                throw new IllegalStateException();
-            }
-        }
-    }
-
-    private static void startRunners(Racer[] runners, Race race) {
-        for (int i = 0; i < runners.length; i++) {
-            runners[i] = new Racer(race, i + 1);
-            runners[i].start();
-        }
-    }
 }
